39f16de4dfce78d0ddf97194d22b4d6e5463c973
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-02-19 18:48:31 -0300

    Atualizações

--- javascript/forgot.js
+++ javascript/forgot.js
@@ -1,19 +1,8 @@
 const inputEmailElement = document.querySelector("#email");
 const buttonForgut = document.querySelector(".button-forgot");
 const mensageErrorElement = document.querySelector("#mensagem-error");
-
-buttonForgut.addEventListener("click", () => {
-    
-    let validateEmail = /\S+@\S+\.\S+/;
-    let yourEmail = validateEmail.test(inputEmailElement.value);
-
-    if (yourEmail) {
-        alert("tudo certo");
-    } else {
-        mensageError();
-    }
-
-})
+const popupElement = document.querySelector('.popup');
+const popupCloseElement = document.querySelector('.popup-close');
 
 const mensageError = () => {
     mensageErrorElement.style.display = "inline-block";
@@ -24,3 +13,33 @@ const mensageError = () => {
         mensageErrorElement.style.display = "none";
     }
 }
+
+// Mostrar popup e fechar popup
+const showPopup = () => {
+    popupElement.style.display = 'block';
+
+    setTimeout(hiddePopup, 5000);
+    function hiddePopup() {
+        popupElement.style.display = 'none';
+    }
+
+}
+
+const hiddePopup = () => {
+    popupElement.style.display = 'none';
+}
+
+popupCloseElement.addEventListener('click', () => hiddePopup());
+
+buttonForgut.addEventListener("click", () => {
+    
+    let validateEmail = /\S+@\S+\.\S+/;
+    let yourEmail = validateEmail.test(inputEmailElement.value);
+
+    if (yourEmail) {
+        showPopup();
+    } else {
+        mensageError();
+    }
+
+})
--- javascript/scripts.js
+++ javascript/scripts.js
@@ -3,7 +3,8 @@ const userPasswordElement = document.querySelector('#password');
 const elementErrorEmail = document.querySelector('#mensagem-error');
 const validateLoginBnt = document.querySelector('.button-submit-login');
 const showPassword = document.querySelector('.show-password');
-console.log(showPassword.children[0])
+const popupElement = document.querySelector('.popup');
+const popupCloseElement = document.querySelector('.popup-close');
 
 let validateEmail = () => userEmailElement.value.trim().length > 4;
 let validatePassword = () => userPasswordElement.value.trim().length > 4;
@@ -14,13 +15,12 @@ const validateLogin = (userEmailElement) => {
     let email = validateEmail.test(userEmailElement.value);
 
     if (email) {
-        elementErrorEmail.style.display = "none";
-        alert("tudo certo");
+        emailValid()
+        showPopup();
     } else {
         return showMensageError();
     }
 
-    
 }
 
 // Adicionar mensagem de erro 
@@ -29,7 +29,7 @@ const showMensageError = () => {
 
     setTimeout(hiddeError, 5000);
 
-    function hiddeError () {
+    function hiddeError() {
         elementErrorEmail.style.display = "none";
     }
 }
@@ -52,26 +52,47 @@ const relaseButtonSingUp = () => {
 
 
 // Mudar para senha visível
-let vez = 0;
+let turn = true;
 const viewPassword = (showPassword) => {
 
-    if (vez === 0) {
+    if (turn) {
         showPassword.children[0].classList.remove('bxs-show');
         showPassword.children[0].classList.add('bxs-hide');
         userPasswordElement.removeAttribute('type');
         userPasswordElement.setAttribute('type', 'text');
-        vez = 1;
+        turn = false;
     } else {
         showPassword.children[0].classList.remove('bxs-hide');
         showPassword.children[0].classList.add('bxs-show');
         userPasswordElement.removeAttribute('type');
         userPasswordElement.setAttribute('type', 'password');
-        vez = 0
+        turn = true;
     }
 
+}
+
+// Email válido
+const emailValid = () => {
+    elementErrorEmail.style.display = "none";
+}
+
+// Mostrar popup e fechar popup
+const showPopup = () => {
+    popupElement.style.display = 'block';
+
+    setTimeout(hiddePopup, 5000);
+    function hiddePopup() {
+        popupElement.style.display = 'none';
+    }
+
+}
+
+const hiddePopup = () => {
+    popupElement.style.display = 'none';
 }
 
 userEmailElement.addEventListener('keyup', () => relaseButtonSingUp());
 userPasswordElement.addEventListener('keyup', () => relaseButtonSingUp());
 showPassword.addEventListener('click', () => viewPassword(showPassword));
 validateLoginBnt.addEventListener('click', () => validateLogin(userEmailElement));
+popupCloseElement.addEventListener('click', () => hiddePopup());
