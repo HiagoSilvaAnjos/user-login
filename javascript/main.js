a781a7b1d434ea22a27c793d9dae43220c90bc9f
Hiago Silva <hiagosilvaanjos7@gmail.com>
2022-03-02 18:16:01 -0300

    corrigi um bug da pagina main

--- javascript/main.js
+++ javascript/main.js
@@ -1,15 +1,17 @@
-const title = document.getElementById('title');
-
-let userLogin = JSON.parse(localStorage.getItem('userLogin'))
-
-title.innerHTML = `Olá ${userLogin.userName}`;
-
 if (localStorage.getItem('token') == null) {
     alert("Você precisa estar logado para acessar esta página!");
     window.location.href = './index.html';
+} else {
+    const title = document.getElementById('title');
+
+    let userLogin = JSON.parse(localStorage.getItem('userLogin'))
+
+    title.innerHTML = `Olá ${userLogin.userName}`;
 }
 
-function logout () {
+
+
+function logout() {
 
     localStorage.removeItem('token');
     localStorage.removeItem('userLogin');
